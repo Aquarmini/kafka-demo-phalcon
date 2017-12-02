5ea4139730a78ca74d5a89571f81cbcf185981da
李铭昕 <715557344@qq.com>
2017-12-02 17:29:36 +0800

    增加测试脚本

--- app/tasks/Kafka/TestTask.php
+++ app/tasks/Kafka/TestTask.php
@@ -3,13 +3,56 @@
 namespace App\Tasks\Kafka;
 
 use App\Tasks\Task;
+use Kafka\ConsumerConfig;
+use Kafka\ProducerConfig;
+use Xin\Phalcon\Logger\Factory;
 
 class TestTask extends Task
 {
 
     public function publishAction()
     {
+        $config = ProducerConfig::getInstance();
+        $config->setMetadataRefreshIntervalMs(10000);
+        $config->setMetadataBrokerList(env('KAFKA_BROKER_LIST'));
+        $config->setBrokerVersion('0.9.0.1');
+        $config->setRequiredAck(1);
+        $config->setIsAsyn(false);
+        $config->setProduceInterval(500);
+        $producer = new \Kafka\Producer(function () {
+            return array(
+                array(
+                    'topic' => 'test',
+                    'value' => 'test....message.',
+                    'key' => 'testkey',
+                ),
+            );
+        });
+        // /** @var Factory $factory */
+        // $factory = di('logger');
+        // $logger = $factory->getLogger('kafka');
+        // $producer->setLogger($logger);
+        $producer->success(function ($result) {
+            dump($result);
+        });
+        $producer->error(function ($errorCode) {
+            dump('error:' . $errorCode);
+        });
+        $producer->send(true);
+    }
 
+    public function consumeAction()
+    {
+        $config = ConsumerConfig::getInstance();
+        $config->setMetadataRefreshIntervalMs(10000);
+        $config->setMetadataBrokerList(env('KAFKA_BROKER_LIST'));
+        $config->setGroupId('test');
+        $config->setBrokerVersion('0.9.0.1');
+        $config->setTopics(array('test'));
+        $consumer = new \Kafka\Consumer();
+        $consumer->start(function ($topic, $part, $message) {
+            dump($message);
+        });
     }
 
 }
