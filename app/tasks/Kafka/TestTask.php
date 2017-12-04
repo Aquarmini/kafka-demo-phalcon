18f9f60b1fbe556db3981d20f46bd379548658bc
李铭昕 <715557344@qq.com>
2017-12-04 16:29:32 +0800

    修改kafka 订阅发布测试

--- app/tasks/Kafka/TestTask.php
+++ app/tasks/Kafka/TestTask.php
@@ -9,50 +9,60 @@ use Xin\Phalcon\Logger\Factory;
 
 class TestTask extends Task
 {
-
     public function publishAction()
     {
-        $config = ProducerConfig::getInstance();
-        $config->setMetadataRefreshIntervalMs(10000);
-        $config->setMetadataBrokerList(env('KAFKA_BROKER_LIST'));
-        $config->setBrokerVersion('1.0.0');
-        $config->setRequiredAck(1);
-        $config->setIsAsyn(false);
-        $config->setProduceInterval(500);
-        $producer = new \Kafka\Producer(function () {
-            return array(
-                array(
-                    'topic' => 'test',
-                    'value' => 'test....message.',
-                    'key' => 'testkey',
-                ),
-            );
-        });
-        // /** @var Factory $factory */
-        // $factory = di('logger');
-        // $logger = $factory->getLogger('kafka');
-        // $producer->setLogger($logger);
-        $producer->success(function ($result) {
-            dump('success:' . $result);
-        });
-        $producer->error(function ($errorCode) {
-            dump('error:' . $errorCode);
-        });
-        $producer->send(true);
+        try {
+            $rcf = new \RdKafka\Conf();
+            $rcf->set('group.id', 'test');
+            $cf = new \RdKafka\TopicConf();
+            $cf->set('offset.store.method', 'broker');
+            $cf->set('auto.offset.reset', 'smallest');
+
+            $rk = new \RdKafka\Producer($rcf);
+            $rk->setLogLevel(LOG_DEBUG);
+            $rk->addBrokers(env('KAFKA_BROKER_LIST'));
+            $topic = $rk->newTopic("test", $cf);
+            for ($i = 0; $i < 1000; $i++) {
+                $topic->produce(0, 0, 'test' . $i);
+            }
+        } catch (Exception $e) {
+            dump('error' . $e->getMessage());
+        }
     }
 
     public function consumeAction()
     {
-        $config = ConsumerConfig::getInstance();
-        $config->setMetadataRefreshIntervalMs(10000);
-        $config->setMetadataBrokerList(env('KAFKA_BROKER_LIST'));
-        $config->setGroupId('test');
-        $config->setBrokerVersion('1.0.0');
-        $config->setTopics(array('test'));
-        $consumer = new \Kafka\Consumer();
-        $consumer->start(function ($topic, $part, $message) {
-            dump($message);
-        });
+        try {
+            $rcf = new \RdKafka\Conf();
+            $rcf->set('group.id', 'test');
+            $cf = new \RdKafka\TopicConf();
+            /*
+                $cf->set('offset.store.method', 'file');
+            */
+            $cf->set('auto.offset.reset', 'smallest');
+            $cf->set('auto.commit.enable', true);
+
+            $rk = new \RdKafka\Consumer($rcf);
+            $rk->setLogLevel(LOG_DEBUG);
+            $rk->addBrokers("127.0.0.1");
+            $topic = $rk->newTopic("test", $cf);
+            //$topic->consumeStart(0, RD_KAFKA_OFFSET_BEGINNING);
+            while (true) {
+                $topic->consumeStart(0, RD_KAFKA_OFFSET_STORED);
+                $msg = $topic->consume(0, 1000);
+                dump($msg);
+                if ($msg->err) {
+                    echo $msg->errstr(), "\n";
+                    break;
+                } else {
+                    echo $msg->payload, "\n";
+                }
+                $topic->consumeStop(0);
+                sleep(1);
+            }
+        } catch (Exception $e) {
+            echo $e->getMessage();
+        }
     }
 
 }
